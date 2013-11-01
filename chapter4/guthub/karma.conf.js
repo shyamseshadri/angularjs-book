8a610e9adf85c369bb50499be7af2148ab37aaab
David Ollerhead <david.ollerhead@gmail.com>
2013-11-01 21:08:43 +0000

    Updating Karma Config to work with updated Karma
    Karma now requires config to be returned as a function, updating code
    to do so.

--- chapter4/guthub/karma.conf.js
+++ chapter4/guthub/karma.conf.js
@@ -1,72 +1,76 @@
 // Testacular configuration
-// Generated on Sun Feb 10 2013 20:46:15 GMT+0530 (IST)
+
+module.exports = function (config) {
+  config.set({
+
+	// base path, that will be used to resolve files and exclude
+	basePath : '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+	// list of files / patterns to load in the browser
+	files : [
+	  JASMINE,
+	  JASMINE_ADAPTER,
+	  'app/scripts/vendor/angular.min.js',
+	  'app/scripts/vendor/angular-resource.min.js',
+	  'test/vendor/angular-mocks.js',
+	  'app/scripts/controllers/*.js',
+	  'app/scripts/services/*.js',
+	  'app/scripts/directives/*.js',
+	  'test/spec/*.js'
+	],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/scripts/vendor/angular.min.js',
-  'app/scripts/vendor/angular-resource.min.js',
-  'test/vendor/angular-mocks.js',
-  'app/scripts/controllers/*.js',
-  'app/scripts/services/*.js',
-  'app/scripts/directives/*.js',
-  'test/spec/*.js'
-];
+	// list of files to exclude
+	exclude : [
+
+	],
 
 
-// list of files to exclude
-exclude = [
-
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+	// test results reporter to use
+	// possible values: 'dots', 'progress', 'junit'
+	reporters : ['progress'],
 
 
-// web server port
-port = 8080;
+	// web server port
+	port : 8080,
 
 
-// cli runner port
-runnerPort = 9100;
+	// cli runner port
+	runnerPort : 9100,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+	// enable / disable colors in the output (reporters and logs)
+	colors : true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+	// level of logging
+	// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+	logLevel : LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch : true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+	// Start these browsers, currently available:
+	// - Chrome
+	// - ChromeCanary
+	// - Firefox
+	// - Opera
+	// - Safari (only Mac)
+	// - PhantomJS
+	// - IE (only Windows)
+	browsers : ['Chrome'],
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+	// If browser does not capture in given timeout [ms], kill it
+	captureTimeout : 5000,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+	// Continuous Integration mode
+	// if true, it capture browsers, run tests and exit
+	singleRun : false
+ 
+  });
+}
